73b132716d7dcb0461b5d86ff58436dabb09b818
iammahbubalam <bubhamnojrin7196@gmail.com>
2025-09-22 23:57:06 +0600

    project init

---
+++ src/test/java/com/iammahbubalam/cp_auth_service/CpAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iammahbubalam.cp_auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
