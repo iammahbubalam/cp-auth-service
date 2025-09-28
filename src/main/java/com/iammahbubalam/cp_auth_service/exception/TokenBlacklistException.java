1b20697931ec54a2c05aa976ff5152457ab4a3e6
iammahbubalam <bubhamnojrin7196@gmail.com>
2025-09-28 09:02:01 +0600

    feat(service): add SecurityKeyManager for loading and caching RSA keys

---
+++ src/main/java/com/iammahbubalam/cp_auth_service/exception/TokenBlacklistException.java
@@ -0,0 +1,7 @@
+package com.iammahbubalam.cp_auth_service.exception;
+
+public class TokenBlacklistException extends RuntimeException{
+    public TokenBlacklistException(String message) {
+        super(message);
+    }
+}
